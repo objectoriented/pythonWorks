6f661f8da9fba2c912cfa2361479290bb0947c0a
Steven-lap <Selarcis@gmail.com>
2017-09-19 07:19:43 -0400

    update00001

---
+++ 2kCharacterSplit/main.py
@@ -0,0 +1,16 @@
+import io
+import string
+import array
+
+stringVar = ""
+arrayVar = []
+varSplit = 2000
+charCount = 0
+
+def TextIn(textString):
+    #https://docs.python.org/3/library/stdtypes.html#str.split
+    splitText = textString.rsplit()
+    for i in textString:
+        print(str(splitText)
+
+TextIn("test")
