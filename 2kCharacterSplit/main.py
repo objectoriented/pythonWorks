a4056ea69bde65bf30a0c7b8aadd016b899f5adb
Steven-lap <Selarcis@gmail.com>
2017-09-23 07:52:47 -0400

    update00002
    now with more string chunks

--- 2kCharacterSplit/main.py
+++ 2kCharacterSplit/main.py
@@ -7,10 +7,15 @@ arrayVar = []
 varSplit = 2000
 charCount = 0
 
-def TextIn(textString):
+def TextIn(textString, charCount=0):
     #https://docs.python.org/3/library/stdtypes.html#str.split
-    splitText = textString.rsplit()
-    for i in textString:
-        print(str(splitText)
+    print(str(textString))
+    for i in str(textString):
+        arrayVar.append(i)
+        charCount += 1
+        #discord counts spaces
 
-TextIn("test")
+    print(str(arrayVar))
+    print(charCount)
+
+TextIn("Python’s standard library is very extensive, offering a wide range of facilities as indicated by the long table of contents listed below. The library contains built-in modules (written in C) that provide access to system functionality such as file I/O that would otherwise be inaccessible to Python programmers, as well as modules written in Python that provide standardized solutions for many problems that occur in everyday programming. Some of these modules are explicitly designed to encourage and enhance the portability of Python programs by abstracting away platform-specifics into platform-neutral APIs.Python’s standard library is very extensive, offering a wide range of facilities as indicated by the long table of contents listed below. The library contains built-in modules (written in C) that provide access to system functionality such as file I/O that would otherwise be inaccessible to Python programmers, as well as modules written in Python that provide standardized solutions for many problems that occur in everyday programming. Some of these modules are explicitly designed to encourage and enhance the portability of Python programs by abstracting away platform-specifics into platform-neutral APIs.Python’s standard library is very extensive, offering a wide range of facilities as indicated by the long table of contents listed below. The library contains built-in modules (written in C) that provide access to system functionality such as file I/O that would otherwise be inaccessible to Python programmers, as well as modules written in Python that provide standardized solutions for many problems that occur in everyday programming. Some of these modules are explicitly designed to encourage and enhance the portability of Python programs by abstracting away platform-specifics into platform-neutral APIs.Python’s standard library is very extensive, offering a wide range of facilities as indicated by the long table of contents listed below. The library contains built-in modules (written in C) that provide access to system functionality such as file I/O that would otherwise be inaccessible to Python programmers, as well as modules written in Python that provide standardized solutions for many problems that occur in everyday programming. Some of these modules are explicitly designed to encourage and enhance the portability of Python programs by abstracting away platform-specifics into platform-neutral APIs.")
