696c3ae91a3d029ee23fe46e7a1636c7014f2aa1
Steven-lap <Selarcis@gmail.com>
2018-05-26 13:59:24 -0400

    Functional
    Splits words into an array from a file (including whitespace) and outputs it into 2000 character posts to a file. TODO: Discord bot

--- 2kCharacterSplit/main.py
+++ 2kCharacterSplit/main.py
@@ -3,7 +3,7 @@ import string
 import array
 import math
 
-userFileIn = input("File name? (do not include dot type): ")
+userFileIn = input("File name? (do not include extention): ")
 textFromFile = io.open((str(userFileIn) + ".txt"), "r+", encoding="utf-8")
 testText = textFromFile.read()
 programFileOut = io.open("testwrite.txt", "a", encoding="utf-8")
@@ -13,29 +13,34 @@ charCount = 0
 charMaxCount = 0
 numPosts = 0
 textCon = 0
+letter = 0
 
 
-def TextIn(textString, charMaxCount=0, charCount=0, textCon=0):
-
+def TextIn(textString, charMaxCount=0, charCount=0, numPosts=0, newPostCounter=0, letter=0, post=True):
     for i in textString:
         # discord counts white spaces
         charMaxCount += 1
         arrayVar.append(i)
     notComplete = True
-    programFileOut.write("\n")
     while(notComplete):
-        if(charCount != charMaxCount):
-            print(str(arrayVar[charCount]), end="")
-            programFileOut.write((str(arrayVar[charCount])))
-            charCount += 1
-        elif((charCount == charMaxCount) and (textCon < 1)):
-            programFileOut.write("\n ------------NEW POST------------")
-            textCon += 1
-
+        if( letter != charMaxCount):
+            #print(str(arrayVar[charCount]), end="")
+            while(post):
+                if ((charCount != varSplit)) and ((charCount + newPostCounter) != charMaxCount):
+                    programFileOut.write((str(arrayVar[letter])))
+                    charCount += 1
+                    letter+=1
+                else:
+                    if (newPostCounter == charMaxCount):
+                        charCount = newPostCounter
+                        post = False
+                        break
+                    programFileOut.write("\n ------------NEW POST------------ \n")
+                    newPostCounter += charCount
+                    charCount = 0
         else:
             numPosts = (math.ceil((len(textString) / 2000)))
             notComplete = False
-
     print("\n")
     print("Number of characters being processed: " + str(charMaxCount))
     print("Number of posts to send entire message: " + str(numPosts))
--- 2kCharacterSplit/printNums.py
+++ 2kCharacterSplit/printNums.py
@@ -7,5 +7,14 @@ maxNum=0
 def printNumsToFile(maxNum, file):
     file = io.open(file, "w+", encoding="utf-8")
     for i in range(maxNum):
-        file.write(str(i+1))
-        file.write("\n")
+        if(i<1):
+            i = 1
+        else:
+            file.write(str(i))
+            file.write(" % ")
+            file.write(str(2000))
+            file.write(" = ")
+            file.write(str(i%2000))
+            file.write("\n")
+
+printNumsToFile(5000, "nums.txt")
